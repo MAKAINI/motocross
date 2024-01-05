3ff403a139ddf1a3d3e6fd6ea0878fb18344d55c
MAKAINI <makainimallh2005@yahoo.com>
2024-01-05 16:51:42 +0100

    modif

---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TrainingController extends Controller
+{
+    public function index()
+    {
+        $trainings = Training::all();
+        return view('training.index', ['trainings' => $trainings]);
+    }
+    public function create()
+    {
+        return view('training.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'length' => 'required|numeric',
+            'max_people' => 'required|numeric',
+            'type' => 'required',
+        ]);
+
+        if ($request->type != 'enfant' && $request->type != 'adulte') {
+            return redirect()->route('training.create');
+        }
+
+        Training::create([
+            "name" => $request->name,
+            "length" => $request->length,
+            "max_people" => $request->max_people,
+            "type" => $request->type
+        ]);
+
+
+        return redirect()->route('training.index');
+    }
+    public function edit_training($id){
+
+
+    }
+
+
+}
